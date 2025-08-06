ed23ad5f1d816cf1155a0ca81777c6a68e980791
Mateus Cesca <165062319+Mcesca@users.noreply.github.com>
2025-08-06 14:36:54 -0300

    Update run.py

--- run.py
+++ run.py
@@ -98,4 +98,5 @@ def enviar():
 # socketio.run(app, host='127.0.0.1', port=80, debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=80, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
+
